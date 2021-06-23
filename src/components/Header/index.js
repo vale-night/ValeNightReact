660394f662797a45db3be142f89b70b8f8e65d7c
caioavelar <43555388+caioavelar@users.noreply.github.com>
2021-06-22 21:53:04 -0300

    Adicionada navegação no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 
 export default () => {
+  const history = useHistory();
   return (
     <header id="header" className="">
-      <i className="fas fa-arrow-left"></i>
+      <i className="fas fa-arrow-left" onClick={() => history.goBack()}></i>
       <img src={logo} alt="Logo" /> 
-      <i className="fas fa-shopping-cart"></i>
+      <Link to="/carrinho">
+        <i className="fas fa-shopping-cart"></i>
+      </Link>
     </header>
 
   )
