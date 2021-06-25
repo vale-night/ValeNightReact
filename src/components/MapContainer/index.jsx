f3b5381d64ad33cae1c9696c7f6bbb0e58d13518
caioavelar <43555388+caioavelar@users.noreply.github.com>
2021-06-25 13:30:26 -0300

    Criação inicial de drawer

--- src/components/MapContainer/index.jsx
+++ src/components/MapContainer/index.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Map, InfoWindow, Marker } from 'google-maps-react';
+import { SwipeableDrawer } from '@material-ui/core';
+React.useLayoutEffect = React.useEffect 
+
 
 export default ({google}) => {
 
@@ -8,16 +11,19 @@ export default ({google}) => {
     const [showInfo, setShowInfo] = useState(false);
     const [marker, setMarker] = useState(null);
     const [image, setImage] = useState('https://via.placeholder.com/50x50');
+    const [drawerOpened, setDrawerOpened] = useState(false);
 
     const onMarkerClick = (props, marker, e) => {
         setMarker(marker);
         setShowInfo(true);
+        setDrawerOpened(true);
     }
 
     const onCloseInfo = () => {
         if (showInfo) {
             setShowInfo(false);
             setMarker(null);
+            setDrawerOpened(false);
         }
     };
 
@@ -30,32 +36,48 @@ export default ({google}) => {
       };
 
     return (
-        <Map
-            google={google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={
-                {
-                    lat: -1.2884,
-                    lng: 36.8233
+        <>
+            <Map
+                google={google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                    {
+                        lat: -1.2884,
+                        lng: 36.8233
+                    }
                 }
-            }
-        >
-            <Marker
-                onClick={onMarkerClick}
-                name={'Festa'}
-                icon = {image}
-            />
-            <InfoWindow
-                marker={marker}
-                visible={showInfo}
-                onClose={onCloseInfo}
             >
-                <div>
-                    <h4> { 'Festa' } </h4>
-                </div>
-            </InfoWindow>
-            
-        </Map>
+                <Marker
+                    onClick={onMarkerClick}
+                    name={'Festa'}
+                    icon = {image}
+                />
+                {/* <InfoWindow
+                    marker={marker}
+                    visible={showInfo}
+                    onClose={onCloseInfo}
+                >
+                </InfoWindow> */}
+                
+            </Map>
+            <SwipeableDrawer
+                anchor="bottom"
+                open={drawerOpened}
+                onOpen={() => console.log('Abriu')}
+                onClose={() => {console.log('fechou')}}
+            >
+                <EventDetails closeDrawerFunction={() => {setDrawerOpened(false)}}/>
+            </SwipeableDrawer>
+        </>
     );
+}
+
+const EventDetails = ({closeDrawerFunction}) => {
+    return (
+        <div>
+            <h4> { 'fiesta' } </h4>
+            <button onClick={closeDrawerFunction}></button>
+        </div>
+    )
 }
